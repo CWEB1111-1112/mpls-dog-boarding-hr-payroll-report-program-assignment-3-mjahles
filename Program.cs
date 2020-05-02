4e3b7749e4c5ae83f04e1db244bb466efdd96b1c
mjahles <ahlmatj@dunwoody.edu>
2020-05-01 23:58:09 -0500

    modified looping structure and added in the break to it. Added in the report of the department gross salary

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace assignment_three
         static void Main(string[] args)
         {
             //Declarations
-            string firstName;
+            string firstName = "";
             string lastName;
             int departmentNumber;
             double salary;
@@ -23,35 +23,44 @@ namespace assignment_three
             double grossSalary;
             string [] departmentName = {"Personnel", "Marketing", "Information Technology", "Computer Services", "Sales", "Legal", "Accounting"};
             double [] departmentSalary = {0, 0, 0, 0, 0, 0, 0,};
+            int x = 0;
             
             //List that contains employee info object instances
             List<Info> employeeInfoList = new List<Info>();
 
             //Intake of employee info
-            Console.WriteLine("Please input the Employee's first name:");
-            firstName = Console.ReadLine();
-            if(firstName != END){
-                Console.WriteLine("Please input the Employee's last name:");
-                lastName = Console.ReadLine();
-                Console.WriteLine("Please input the Employee's department number:");
-                Console.WriteLine("Personnel [1], Marketing [2], Information Technology [3], Computer Services [4], Sales [5], Legal [6], Accounting [7]");
-                departmentNumber = Convert.ToInt32(Console.ReadLine());
-                    //Defensive programming to ensure correct department number
-                    while(departmentNumber !=1 && departmentNumber !=2 && departmentNumber !=3 && departmentNumber !=4 && departmentNumber !=5 && departmentNumber !=6 && departmentNumber !=7){
-                        Console.WriteLine("Please input a valid Department number:");
-                        Console.WriteLine("Personnel [1], Marketing [2], Information Technology [3], Computer Services [4], Sales [5], Legal [6], Accounting [7]");
-                        departmentNumber = Convert.ToInt32(Console.ReadLine());
-                    }
-                Console.WriteLine("Please input the Employee's salary:");
-                salary = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Please input the Employee's hours worked:");
-                hoursWorked = Convert.ToInt32(Console.ReadLine());
-                grossSalary = hoursWorked * salary;
-                
-                Info anEmployee = new Info(firstName, lastName, departmentNumber, salary, hoursWorked, grossSalary);  //Creating the new Info object
-                employeeInfoList.Add(anEmployee); //Adding the employee to the list using the Add() method
-                Console.WriteLine($"Employee's Name: {firstName} {lastName}\nGross salary: {grossSalary}\nDepartment Name: {departmentName[departmentNumber - 1]}");
+            while(x < x + 1){
+                x++;
+                Console.WriteLine("Please input the Employee's first name:");
+                firstName = Console.ReadLine();
+                if(firstName != END){    
+                    Console.WriteLine("Please input the Employee's last name:");
+                    lastName = Console.ReadLine();
+                    Console.WriteLine("Please input the Employee's department number:");
+                    Console.WriteLine("Personnel [1], Marketing [2], Information Technology [3], Computer Services [4], Sales [5], Legal [6], Accounting [7]");
+                    departmentNumber = Convert.ToInt32(Console.ReadLine());
+                        //Defensive programming to ensure correct department number
+                        while(departmentNumber !=1 && departmentNumber !=2 && departmentNumber !=3 && departmentNumber !=4 && departmentNumber !=5 && departmentNumber !=6 && departmentNumber !=7){
+                            Console.WriteLine("Please input a valid Department number:");
+                            Console.WriteLine("Personnel [1], Marketing [2], Information Technology [3], Computer Services [4], Sales [5], Legal [6], Accounting [7]");
+                            departmentNumber = Convert.ToInt32(Console.ReadLine());
+                        }
+                    Console.WriteLine("Please input the Employee's salary:");
+                    salary = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Please input the Employee's hours worked:");
+                    hoursWorked = Convert.ToInt32(Console.ReadLine());
+                    grossSalary = hoursWorked * salary;
+                    
+                    Info anEmployee = new Info(firstName, lastName, departmentNumber, salary, hoursWorked, grossSalary);  //Creating the new Info object
+                    employeeInfoList.Add(anEmployee); //Adding the employee to the list using the Add() method
+                    Console.WriteLine($"Employee's Name: {firstName} {lastName}\nGross salary: {grossSalary}\nDepartment Name: {departmentName[departmentNumber - 1]}");
+                    departmentSalary[departmentNumber-1] = grossSalary + departmentSalary[departmentNumber-1];
+                }
+                else{
+                    break;
+                }
             }
+            Console.WriteLine($"{departmentName[0]} Gross Salary: {departmentSalary[0]}\n{departmentName[1]} Gross Salary: {departmentSalary[1]}\n{departmentName[2]} Gross Salary: {departmentSalary[2]}\n{departmentName[3]} Gross Salary: {departmentSalary[3]}\n{departmentName[4]} Gross Salary: {departmentSalary[4]}\n{departmentName[5]} Gross Salary: {departmentSalary[5]}\n{departmentName[6]} Gross Salary: {departmentSalary[6]}");
         }
     }
     //Separate class used for making the List type
